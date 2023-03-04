fada1f9f0fad46a49a65b33224c6ef1413d1b2d3
tecknovice <tecknovice@gmail.com>
2023-03-04 18:35:27 +0700

    feature: display resume

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,32 @@
 import { Box, Paper } from "@mui/material";
-import { useEffect } from "react";
 import { useForm, Controller, FormProvider } from "react-hook-form";
 import Forms from "./components/forms";
+import { ResumeComponent } from "./components/resume";
 import { Resume } from "./interfaces/resume";
 
 function App() {
   const methods = useForm<Resume>();
   const { watch, getValues, handleSubmit } = methods;
   const onSubmit = (data: Resume) => console.log(data);
-  useEffect(() => {
-    const data = getValues();
-    console.log({ data });
-  }, [watch()]);
+
   return (
-    <Box bgcolor="#e6e6e6" display="flex" minHeight="100vh">
-      <Box width="25%">
-        <Paper
-          elevation={5}
-          sx={{ width: "100%", minHeight: "100%", borderRadius: 0 }}
-        >
-          <FormProvider {...methods}>
+    <FormProvider {...methods}>
+      <Box bgcolor="#e6e6e6" display="flex" minHeight="100vh">
+        <Box width="25%">
+          <Paper
+            elevation={5}
+            sx={{ width: "100%", minHeight: "100%", borderRadius: 0 }}
+          >
             <form onSubmit={handleSubmit(onSubmit)}>
               <Forms />
             </form>
-          </FormProvider>
-        </Paper>
+          </Paper>
+        </Box>
+        <Box width="75%" padding={"50px 100px"}>
+          <ResumeComponent />
+        </Box>
       </Box>
-      <Box width="75%" padding={"50px 100px"}>
-        <Paper
-          elevation={5}
-          sx={{ width: "100%", minHeight: "100%", borderRadius: 0 }}
-        ></Paper>
-      </Box>
-    </Box>
+    </FormProvider>
   );
 }
 
---
+++ src/components/resume/index.tsx
@@ -0,0 +1,110 @@
+import {
+  Box,
+  Divider,
+  ListItem,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useEffect } from "react";
+import { Controller, useFormContext } from "react-hook-form";
+import { GitHub, LinkedIn } from "@mui/icons-material";
+import { Resume } from "../../interfaces/resume";
+
+export function ResumeComponent() {
+  const { getValues, watch } = useFormContext<Resume>();
+  useEffect(() => {
+    const data = getValues();
+    console.log({ data });
+  }, [watch()]);
+  return (
+    <Paper
+      elevation={5}
+      sx={{
+        width: "100%",
+        minHeight: "100%",
+        borderRadius: 0,
+        padding: "16px 64px",
+      }}
+    >
+      {getValues("information") && (
+        
+          <Box
+            display={"flex"}
+            flexDirection="column"
+            alignItems={"center"}
+            py="8px"
+          >
+            <Typography variant="h3">
+              {getValues("information.name")}
+            </Typography>
+            <Typography variant="subtitle1">
+              {getValues("information.mobile")}
+            </Typography>
+            <Typography variant="subtitle1">
+              {getValues("information.email")}
+            </Typography>
+            {getValues("information.github") && (
+              <Box display={"flex"} alignItems="center">
+                <GitHub />
+                <Typography variant="subtitle1">
+                  {getValues("information.github")}
+                </Typography>
+              </Box>
+            )}
+            {getValues("information.linkedIn") && (
+              <Box display={"flex"} alignItems="center">
+                <LinkedIn />
+                <Typography variant="subtitle1">
+                  {getValues("information.linkedIn")}
+                </Typography>
+              </Box>
+            )}
+          </Box>
+      )}
+      {getValues("summary") && (
+        <Box py="8px">
+          <Typography variant="subtitle1" textAlign={"center"}>
+            {getValues("summary")}
+          </Typography>
+          <Divider />
+        </Box>
+      )}
+      {getValues("experiences") && (
+        <Typography variant="h5">Experiences</Typography>
+      )}
+      {getValues("experiences")?.map((exp, index) => (
+        <Box py="8px" key={index}>
+          <Typography fontWeight="bold">{exp.company}</Typography>
+          <Box display={"flex"}>
+            <Typography marginRight={1}>{exp.jobTitle}</Typography>
+            <Typography>{exp.period}</Typography>
+          </Box>
+          {exp.jobDescription && (
+            <ListItem sx={{ display: "list-item" }}>
+              {exp.jobDescription}
+            </ListItem>
+          )}
+        </Box>
+      ))}
+      {getValues("education") && (
+        <Typography variant="h5">Education</Typography>
+      )}
+      {getValues("education")?.map((edu, index) => (
+        <Box py="8px" key={index}>
+          <Typography fontWeight="bold">{edu.university}</Typography>
+          <Box display={"flex"}>
+            <Typography marginRight={1}>{edu.faculty}</Typography>
+            {edu.gpa && <Typography>GPA: {edu.gpa}</Typography>}
+          </Box>
+        </Box>
+      ))}
+      {getValues("skills") && <Typography variant="h5">Skills</Typography>}
+      {getValues("skills")?.map((skill, index) => (
+        <ListItem sx={{ display: "list-item" }} key={index}>
+          {skill.name}
+        </ListItem>
+      ))}
+    </Paper>
+  );
+}
